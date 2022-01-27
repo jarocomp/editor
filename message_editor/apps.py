d186220af8baa2dad23bf6abff423787c3e33379
Jaro <petrovskyjaroslav@gmail.com>
2022-01-27 10:46:28 +0100

    added style

--- message_editor/apps.py
+++ message_editor/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class MessageEditorConfig(AppConfig):
     name = 'message_editor'
--- message_editor/tests.py
+++ message_editor/tests.py
@@ -16,8 +16,6 @@ class  UserTest(TestCase):
         user_count = Message.objects.all().count()
         self.assertEqual(user_count, 1)
 
-
-
     def url_is_resolved(self):
 
         self.assertEqual(resolve(reverse('messages')).func,show_all_messages)
